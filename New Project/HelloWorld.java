0823c53211202889561582ddb8f1b353ff8010c8
vovkorol <vovkorol@users.noreply.github.com>
2015-07-31 10:46:32 +0300

    Created by Coding Ground Client

---
+++ New Project/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int mas [] = new int [10];
+        for (int i = 0; i < 10; i++){
+            mas [i] = i + 1;
+            System.out.println (mas [i]);
+        }
+     }
+}
